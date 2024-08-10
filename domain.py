76dd20bb34c11b6d86fd1fb94c4611fef837b373
Alan Martines <alancpmartines@hotmail.com>
2024-08-10 12:02:37 -0400

    Update domain.py
    Update domain.py

--- domain.py
+++ domain.py
@@ -11,33 +11,38 @@ def execCommand(command):
     popen.stdout.close()
     return retStdOut
 
-cmd_output=execCommand("whois "+sys.argv[1])
-# cmd_output=execCommand("whois argohost.net")
+cmd_output = execCommand("whois " + sys.argv[1])
+
+ano, mes, dia = None, None, None
 
 for line in cmd_output:
-    m = re.match(".*Expiry\sDate\:\s(\d{4})\-(\d{2})\-(\d{2})T.*",line)
+    m = re.match(r".*Expiry\sDate\:\s(\d{4})\-(\d{2})\-(\d{2})T.*", line)
     if m:
-        ano=m.group(1)
-        mes=m.group(2)
-        dia=m.group(3)
-    m = re.match(".*expires\:\s+(\d{4})(\d{2})(\d{2}).*",line)
+        ano = m.group(1)
+        mes = m.group(2)
+        dia = m.group(3)
+    m = re.match(r".*expires\:\s+(\d{4})(\d{2})(\d{2}).*", line)
     if m:
-        ano=m.group(1)
-        mes=m.group(2)
-        dia=m.group(3)
-    m = re.match("Expires on\.+\s*:\s*(\d{4})-(\w{3})-(\d{2})\.",line)
+        ano = m.group(1)
+        mes = m.group(2)
+        dia = m.group(3)
+    m = re.match(r"Expires on\.+\s*:\s*(\d{4})-(\w{3})-(\d{2})\.", line)
     if m:
-        ano=m.group(1)
-        mes=m.group(2)
-        dia=m.group(3)
+        ano = m.group(1)
+        mes = m.group(2)
+        dia = m.group(3)
 
-stringdate=ano+"-"+mes+"-"+dia
-try:
-   expiration_date=datetime.datetime.strptime(stringdate,"%Y-%m-%d")
-except:
-   expiration_date=datetime.datetime.strptime(stringdate,"%Y-%b-%d")
-now=datetime.datetime.now()
-remaining_time=expiration_date-now
-remaining_days=str(remaining_time.total_seconds()/86400).split(".")[0]
+if ano and mes and dia:
+    stringdate = ano + "-" + mes + "-" + dia
+    try:
+        expiration_date = datetime.datetime.strptime(stringdate, "%Y-%m-%d")
+    except ValueError:
+        expiration_date = datetime.datetime.strptime(stringdate, "%Y-%b-%d")
 
-print (remaining_days)
+    now = datetime.datetime.now()
+    remaining_time = expiration_date - now
+    remaining_days = str(remaining_time.total_seconds() / 86400).split(".")[0]
+
+    print(remaining_days)
+else:
+    print("Data de expiração não encontrada.")
